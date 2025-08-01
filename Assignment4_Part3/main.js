49a916afd51794ced1a49ec02ab0aeb7ced5b306
anjolaadejorodc <anjolaoluwa.adejoro@dcmail.ca>
2025-07-31 21:49:40 -0400

    Final Commit
    Commit 5

--- Assignment4_Part3/main.js
+++ Assignment4_Part3/main.js
@@ -32,3 +32,46 @@ Ball.prototype.draw = function () {
   ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
   ctx.fill();
 };
+
+// Update ball position and bounce off edges
+Ball.prototype.update = function () {
+  if ((this.x + this.size) >= width || (this.x - this.size) <= 0) {
+    this.velX = -this.velX;
+  }
+
+  if ((this.y + this.size) >= height || (this.y - this.size) <= 0) {
+    this.velY = -this.velY;
+  }
+
+  this.x += this.velX;
+  this.y += this.velY;
+};
+
+const balls = [];
+
+while (balls.length < 25) {
+  const size = random(10, 20);
+  const ball = new Ball(
+    random(size, width - size),
+    random(size, height - size),
+    random(-7, 7),
+    random(-7, 7),
+    randomRGB(),
+    size
+  );
+  balls.push(ball);
+}
+function loop() {
+  // Clear canvas with a semi-transparent background for trail effect
+  ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
+  ctx.fillRect(0, 0, width, height);
+
+  for (const ball of balls) {
+    ball.draw();
+    ball.update();
+  }
+
+  requestAnimationFrame(loop);
+}
+
+loop(); // Start animation
