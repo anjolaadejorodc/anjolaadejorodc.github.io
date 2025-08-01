e0502601c3113c87978150a5e57ed336c8f391a9
anjolaadejorodc <anjolaoluwa.adejoro@dcmail.ca>
2025-07-31 23:58:02 -0400

    Commit 3
    third commit

--- Assignment4_Part4/main-finished.js
+++ Assignment4_Part4/main-finished.js
@@ -57,20 +57,20 @@ class Ball {
   }
 
 
-  collisionDetect() {
-    for (const ball of balls) {
-      if (!(this === ball)) {
-        const dx = this.x - ball.x;
-        const dy = this.y - ball.y;
-        const distance = Math.sqrt(dx * dx + dy * dy);
+collisionDetect() {
+  for (const ball of balls) {
+    if (!(this === ball)) {
+      const dx = this.x - ball.x;
+      const dy = this.y - ball.y;
+      const distance = Math.sqrt(dx * dx + dy * dy);
 
-        if (distance < this.size + ball.size) {
-          ball.color = this.color = randomRGB();
-        }
+      if (distance < this.size + ball.size) {
+        this.color = ball.color = randomRGB();
       }
     }
   }
-}
+
+
 
 const balls = [];
 
