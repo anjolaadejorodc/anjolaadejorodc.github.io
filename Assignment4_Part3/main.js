52fd82da11f92bec3699535820556613a019ad29
anjolaadejorodc <anjolaoluwa.adejoro@dcmail.ca>
2025-07-31 21:46:19 -0400

    Commit 3
    Third Commit

--- Assignment4_Part3/main.js
+++ Assignment4_Part3/main.js
@@ -1,19 +1,16 @@
-// setup canvas
-
+// Setup canvas
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
 
-// function to generate random number
-
+// Generate random number
 function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// function to generate random color
-
+// Generate random color
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
