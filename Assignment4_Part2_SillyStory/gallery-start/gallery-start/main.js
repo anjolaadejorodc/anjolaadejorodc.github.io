be13748f709ae2fbcc7bbfcb05a615a4cee36cc0
anjolaadejorodc <anjolaoluwa.adejoro@dcmail.ca>
2025-07-31 21:17:06 -0400

    Commit 2
    Declaring the array of image filenames and declaring the alternative text for each image file

--- Assignment4_Part2_SillyStory/gallery-start/gallery-start/main.js
+++ Assignment4_Part2_SillyStory/gallery-start/gallery-start/main.js
@@ -6,7 +6,18 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 
+const imageList = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
+
+
 /* Declaring the alternative text for each image file */
+
+const altText = {
+  'pic1.jpg': 'Closeup of a blue human eye',
+  'pic2.jpg': 'Rock that looks like a wave',
+  'pic3.jpg': 'Purple and white pansies',
+  'pic4.jpg': 'Section of wall from a pharaoh’s tomb',
+  'pic5.jpg': 'Large moth on a leaf'
+};
 
 /* Looping through images */
 
