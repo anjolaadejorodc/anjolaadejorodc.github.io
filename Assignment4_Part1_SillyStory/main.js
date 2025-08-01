5ad0a23bb380fa1ee4300dfd96f8151ff4ca79a1
anjolaadejorodc <anjolaoluwa.adejoro@dcmail.ca>
2025-07-31 21:00:53 -0400

    Commit 3
    Set up DOM variables and random text arrays in JS

--- Assignment4_Part1_SillyStory/main.js
+++ Assignment4_Part1_SillyStory/main.js
@@ -2,3 +2,19 @@
 // File: main.js
 // Date: 01 August 2025
 // JavaScript logic for the silly story generator
+
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+function randomValueFromArray(array) {
+  const randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
+}
+
+const storyText =
+  'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
+
+const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
+const insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
+const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
